7e2c0419ce43ac55899cace3589f7195206a8ab7
waleed-02 <146323385+waleed-02@users.noreply.github.com>
2024-02-11 15:35:15 +0000

    container config done

--- 01_basics/02_data_types.js
+++ 01_basics/02_data_types.js
@@ -1,6 +1,29 @@
-"use strict"; // treat all js code as newer version of js 
-// alert("hello"); 
+"use strict"; // treat all js code as newer version of js
+// alert("hello");
 // it will show error as we are using nodejs, not browser
 // to run this statement in nodejs the sintax is different
-console.log(3+3); 
+console.log(3 + 3);
 console.log("alex");
+
+// data types
+let name = "alex"; // string data type
+let age = 20; // number data type
+let isValid = true; // boolean data type
+
+// primitine data types
+// number ==> data type range in java script is 2 to the power 53
+// if it exceeds we have bigint
+// string ==> "" or ''
+// boolean ==> either true or false
+// null ==> standalone value
+let content = null;
+// its type will be an object not null. it is an unproved theory till now what is the type of null data type.
+// undefined ==>
+// symbol ==> uniqueness
+
+// non-primitive data types or reference data types
+// object
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
