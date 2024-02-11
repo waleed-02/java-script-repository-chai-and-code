ab79e701676a978f3072adac3b31619ab148782c
waleed-02 <146323385+waleed-02@users.noreply.github.com>
2024-02-11 15:04:13 +0000

    container config done

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,6 @@
+"use strict"; // treat all js code as newer version of js 
+// alert("hello"); 
+// it will show error as we are using nodejs, not browser
+// to run this statement in nodejs the sintax is different
+console.log(3+3); 
+console.log("alex");
