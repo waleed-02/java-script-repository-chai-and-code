8c6977c52caeedada0a8f304f3c3a2171ab66846
waleed-02 <146323385+waleed-02@users.noreply.github.com>
2024-02-13 08:31:55 +0000

    conversion of data types are done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,86 @@
+// // conversion into Number of different value 
+// let score = 33;// it is a default way to declare a number 
+// // let score = "33abc";// it will give NaN value but its type will be number
+// // let score = 33; // it will give 33
+// // let score = "33"; // it will give 33
+// // let score = null //it will give zero
+// // let score = undefined;  // it will give NaN
+// // let score = false ; // it will give 1 on true and 0 on false
+// let score=[22,"alex"];// it will give NaN
+// console.log(typeof score);
+// let ValueInNumber = Number(score);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber); // it will give NaN means not a number but its type will be number
+
+// NaN(not a number) its type will be a number  but the output will be as follows shown below
+// value ==> output
+//  33 ==> 33
+// "33abc" ==> NaN
+//  "alex" ==> NaN
+// true ==> 1
+// null ==> 0
+// undefined ==> NaN 
+// [22,"alex"] ==> Nan
+
+// conversion into boolean of different values
+//  let isloggedin= true; // its the default method to do it
+
+//  let isloggedin= 1;
+// let isloggedin="";// it will give false
+// let isloggedin ="alex";// it will give true
+// let isloggedin=null;// it will give false
+// let isloggedin =undefined;// it will give false
+// let isloggedin = "01874jha";// it will give true
+// let isloggedin="fajf0293";// it will give true
+// let isloggedin=true;// it will give true
+// let isloggedin=[22,'alex'];// it will give true
+// let isloggedin=[0];// it will give true
+// let isloggedin =[true]// it will give true
+// let isloggedin =[]// it will give true
+// // it will give ture on any value of number negative or positive except for zero only it will be false in terms of values in numbers
+// console.log(typeof(isloggedin));// it will give number as its type
+// console.log(isloggedin);// it will give 1 as its value on output
+// let isConnected = Boolean(isloggedin); // conversion into boolean as its type from number
+// console.log(typeof(isConnected)); // its type will be boolean
+// console.log(isConnected); // its value will be true on output
+
+// value ==> converesion true or false
+// 1 ==> true
+// false ==> false
+// 0 ==> false
+// "" ==> false
+// null ==> false
+// undefined ==> false
+// "alex" ==> true
+// "23909jkf" ==> true
+// "jkf4528" ==> true
+// true ==> true
+// [22,"alex"] ==> true
+// [0] ==> true
+// [] ==> true
+// [true] ==> true
+
+
+// lets convert different values into strings
+// let nameis="alex";// it will give string as is default type is string only
+// let nameis=23; // it will give 23 
+// let nameis="";// it will give nothing but its type will be a string
+// let nameis=null;// it will give null 
+// let nameis = undefined;// it will give undefined but its type after conversion will be a string
+// let nameis= true;// it will give true
+// let nameis=[22,"alex"];//it will give 22,alex and its type will be a string after conversion by defautl it is a object only
+
+
+// console.log(typeof nameis);
+// console.log(nameis);
+// let isname= String(nameis)
+// console.log(typeof isname);
+// console.log(isname)
+// values ==> output the type will be string as we are conversing the type of them
+// "alex"==> alex
+// true ==> true
+// null ==> null
+// undefined ==> undefined
+// "" ==> it will not print any thing
+// [22,"alex"] ==> 22,alex
+// 22 ==> 22
