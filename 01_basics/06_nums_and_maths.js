dbdb1bc01c84df07108905ec2379ebd5f3d5c57e
waleed-02 <146323385+waleed-02@users.noreply.github.com>
2024-02-20 14:56:27 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,44 @@
+// const score = 300;
+// const balance = new Number(score);
+// console.log(score);
+// console.log(typeof score);
+// console.log(balance);
+// console.log(typeof balance);
+// // number to string conversion
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(4));
+
+// const othernum = 1123.49989445;
+// console.log(othernum.toPrecision(7));
+
+// const hundreds = 10000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+// Number.MAX_SAFE_INTEGER;
+// Number.MAX_VALUE;
+// Number.MIN_VALUE;
+// Number.MIN_SAFE_INTEGER;
+
+
+// ***********maths********************
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.sqrt(16));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(3.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.pow(2,2));
+// console.log(Math.min(35,6,7,74,3));
+// console.log(Math.max(35,6,7,74,3));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random() * (max-min +1)  )+min);
+
+
