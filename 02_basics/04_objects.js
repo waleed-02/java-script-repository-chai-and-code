2ed418429ef2c10f8449b079f333355504881bb7
waleed-02 <146323385+waleed-02@users.noreply.github.com>
2024-02-25 15:02:41 +0000

    objects done

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,58 @@
+// const app= new Object(); // it is a single ton object 
+const apps = {}; // it is a non single ton object
+
+apps.id="key12";
+apps.name="alex";
+apps.isonline=false;
+
+// console.log(apps);
+
+const user = {
+    email:"alex@gmail.com",
+    fullname:{
+        username:{
+            userfirstname:"alex",
+            userlastname:"potter",
+        }
+    }
+};
+// console.log(user.fullname.username.userfirstname);
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"c",4:"d"};
+const obj4={5:"c",6:"d"};
+const obj5={7:"c",8:"d"};
+const obj6={9:"c",10:"f"};
+// const obj3 = {obj1,obj2}// it will show problem of array
+// const obj3 = Object.assign({},obj1,obj2,obj4,obj5,obj6);
+const obj3 = { ...obj1,...obj2,...obj4,...obj5,...obj6 };
+
+console.log(obj3);
+
+const users =
+[
+    {
+        id:1,
+        email:"alex@gmail.com",
+        
+    },
+    {
+        id:1,
+        email:"alex@gmail.com",
+
+    },
+    {
+        id:1,
+        email:"alex@gmail.com",
+
+    }
+]
+
+users[1].email
+console.log(user);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+console.log(user.hasOwnProperty("isonline"));
